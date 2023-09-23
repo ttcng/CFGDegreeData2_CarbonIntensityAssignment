a09fc236d6e39e141774e2031af6d01367260471
ttcng <ttcng@outlook.com>
2023-09-23 22:15:22 +0100

    Parsing website data to make postcode dictionary

--- Input file reader.py
+++ Input file reader.py
@@ -1,39 +1,25 @@
 # Function for parsing the numbers
-def numberParser(someString):
-    output = []
-    asList = someString.split(",")
+def townParser(someString, region, areaID):
+    townToRegion = {}
+    asList = someString.split(", ")
     print(asList)
-    for numberSet in asList:
-        if len(numberSet) > 2:  # is XY - XZ
-            newNumberSet = numberSet.replace(' - ', ",")
-            startEnd = newNumberSet.split(",")
-            for j in range(len(startEnd)):
-                startEnd[j] = int(startEnd[j])
-            output.append(startEnd)
-        else:  # is a number
-            output.append(int(numberSet))
-    return output
+    for town in asList:
+        townToRegion[town] = {'region': region,
+                              'areaID': areaID}
+    return townToRegion
 
-with open('10 Eastern Area.txt') as f:
-    lines = f.readlines()[1:] #Excluding title
+with open('townsData/10 Eastern Area.txt') as f:
+    lines = f.readlines() #Excluding title
     print(lines)
 
-    # Removing line breaks \n
     useList = []
     for line in lines:
        useList.append(line[:-1])
-    print(useList)
 
-    #Making a dictionary of postcodes for the region
-    postcodes = {}
-    for i in range(len(useList)):
-        print(useList[i])
-        if i % 2 == 0: #is the letter part of postcode
-            postcodes[useList[i]] = ""
-        else: #is number part of postcode
-            postcodeNumber = numberParser(useList[i])
-            #Adding to dictionary
-            postcodes[useList[i-1]] = postcodeNumber
+    region = useList[0]
+    areaID = int(useList[1])
+    towns = useList[2]
 
-    print(postcodes)
+    print(townParser(towns,region,areaID))
+
 
--- Python Assignment Carbon Intensity.py
+++ Python Assignment Carbon Intensity.py
@@ -1,5 +1,16 @@
-# Installation instructions
-'''
+"""
+----------------------------------------------------
+Data used from:
+Distribution Network Areas: Energy Solutions: Understanding PES Distributor Areas: https://www.energybrokers.co.uk/electricity/PES-Distributor-areas
+Carbon Intensity: https://carbonintensity.org.uk/
+
+Using the Carbon Intensity API provided by National Grid ESO, in partnership with Environmental Defense Fund Europe, 
+University of Oxford Department of Computer Science and WWF.
+
+This API provides the history for each region in Great Britain. The user inputs a town of their choice, which is used to 
+find the associated region and its carbon intensity across the period of a chosen day.
+----------------------------------------------------
+Installation Instructions
 To check if the Requests module is already installed, navigate to the Python Command Prompt and write:
 python -m pip show requests.
 To install the Requests module, use 'python -m pip install requests' in the Command Prompt.
@@ -7,41 +18,137 @@ To install the Requests module, use 'python -m pip install requests' in the Comm
 Likewise for json and matplotlib, respectively:
 'python -m pip install json'
 'python -m pip install matplotlib'
-'''
+"""
+import os  # Package for finding relative directory
+import requests  # Module required for API requests
+import matplotlib.pyplot as plt  # Module required for making graphs
+import datetime
 
-'''
-Data used from 
-Distribution Network Areas: Energy Solutions: Understanding PES Distributor Areas: https://www.energybrokers.co.uk/electricity/PES-Distributor-areas
-Carbon Intensity: https://carbonintensity.org.uk/
-'''
-import requests #Module required for API requests
-import json #Module required for parsing JSON from API call
-import matplotlib #Module required for making graphs
+# Reading text files in to create dictionary of town vs region.
+# Dictionary for corresponding area IDs (usedby electricity distributors) and region IDs (used by API)
 
-#Finding which region the postcode belongs to
-#Region to postcode mapping data from Energy Solutions website
-
-regions = {
-    'East England': {
-        'areaID': 10,
-        'disNetwork': 'Eastern Electric',
-        'postcodes': {'AL': [[1, 10]], 'CB': [[1, 11]], 'CM': [[0, 24]], 'CO': [[1, 16]], 'E': [4], 'EN': [[1, 11]], 'HA': [[0, 9]], 'HP': [[1, 8]], 'IG': [10], 'IP': [[1, 33]], 'LU': [[1, 7]], 'MK': [[44, 45]], 'N': [9, [11]], 'NR': [[1, 35]], 'NW': [2, 4, 7], 'PE': [[30, 38]], 'RM': [[1, 18]], 'G': [[1, 19]], 'SS': [[11, 17]], 'WD': [[1, 7]]}
-    }
-    'London'
-
-    }
+correspondingID = {
+    10: 10,  # area ID to region ID
+    11: 9,
+    12: 13,
+    13: 6,
+    14: 8,
+    15: 4,
+    16: 3,
+    17: 1,
+    18: 2,
+    19: 14,
+    20: 12,
+    21: 7,
+    22: 11,
+    23: 5,
 }
 
+# Setting up dictionary for town to region
+townsToRegion = {}
+
+# Function for adding each town to the dictionary
+def townParser(someString, regionID, region):
+    asList = someString.split(", ")
+    for town in asList:
+        townsToRegion[town] = {"regionID": regionID, "regionName": region}
 
 
+fileList = os.listdir("townsData")
+for fileName in fileList:
+    areaID = fileName[:2]
+    region = fileName[3:-4]
+    tempRegionID = correspondingID[int(areaID)]
 
-# headers = {
-#     'Accept': 'application/json'
-# }
-#
-# r = requests.get('https://api.carbonintensity.org.uk/intensity/date', params={}, headers=headers)
-#
-# data = r.json()
-#
-# print(json.dumps(data, indent=4))
+    with open("townsData/{}".format(fileName)) as f:
+        towns = f.readlines()[
+            4
+        ]  # Excluding first three lines as turns out we don't need those
+        townParser(towns, tempRegionID, region)
 
+# User input to get town and check in dictionary
+townData = None  # Wasn't a global variable so can use here
+while townData is None:
+    userInput = input("Enter a valid town in Britain: ")
+    choice = userInput.title()
+    townData = townsToRegion.get(choice)
+
+regionID = townData["regionID"]
+print(f'Your town is in the {region}.')
+
+# Choice for API calls
+date = input(
+    "Please choose a date in the format dd/mm/yyyy or type 'now' for today's forecast: "
+)
+if len(date) == 3:
+    date1 = str(datetime.date.today())
+else:
+    date1 = date[-4:] + "-" + date[3:5] + "-" + date[:2]
+
+date2 = date1[:-1] + str(int(date1[-1]) + 1)
+
+
+# Calling the API to get the data points for every half hour
+def callAPI(regionID, date1, date2):
+    headers = {"Accept": "application/json"}
+
+    r = requests.get(
+        "https://api.carbonintensity.org.uk/regional/intensity/{}T00:01Z/{}T00:00Z/regionid/{}".format(
+            date1, date2, regionID
+        ),
+        params={},
+        headers=headers,
+    )
+    data = r.json()
+    return data
+
+
+# Calling API
+data = callAPI(regionID, date1, date2)
+
+# Getting total carbon intensity per half hour
+info = data["data"]
+dnoRegion = info["dnoregion"]
+region = info["shortname"]
+dataPoints = []
+times = []
+for halfHour in info["data"]:
+    timePoint = halfHour["from"][-6:-1]  # Getting just the times for the date in question
+    times.append(timePoint)
+    intensity = halfHour["intensity"]["forecast"]
+    dataPoints.append(intensity)
+
+graphName = f"Carbon Intensity on {date1} for {region}"
+
+# Writing to a file
+with open("results.txt", "a") as f:
+    f.write(
+        f"\n\nResults for {choice} on {datetime.date.today()}\n"
+        f"The town chosen was {choice}.\n"
+        + f"This town is in {region}, which has the electricity provider, {dnoRegion}.\n\n"
+        + f"{graphName}:\n\n"
+        + "Time   |  Carbon Intensity (gCO2/kWh)\n"
+        + "-------------------------------------\n"
+    )
+
+    for i in range(len(times)):
+        f.write(f"{times[i]}  |               {dataPoints[i]}\n")
+
+print("The results file has been updated.")
+
+# Showing plot
+x_axis = times
+y_axis = dataPoints
+plt.plot(x_axis, y_axis)
+plt.title(graphName)
+plt.xlabel("Time")
+plt.ylabel("Carbon Intensity in gCO2/kWh")
+
+plt.xticks(rotation=45, ha="right")
+
+# Getting every other tickmark on the hours instead of every half hour
+a = plt.gca()
+ax = a.axes
+ax.set_xticks(ax.get_xticks()[::2])
+
+plt.show()
